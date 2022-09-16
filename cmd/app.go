2f65c0c887aa98d537c8bfa2a9ee7d3a51fb0399
garet2gis <br-avm32@yandex.ru>
2022-09-16 13:30:08 +0300

    feat: add token getting from flags

--- cmd/app.go
+++ cmd/app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"tg_customers_bot/internal/config"
 	"tg_customers_bot/pkg/logging"
 )
@@ -11,4 +12,22 @@ func main() {
 	logger.Debug("Start application")
 	cfg := config.GetConfig()
 	logger.Debugf("Config: %+v", cfg)
+	token := mustToken(logger)
+	logger.Debugf("Token: %s", token)
+}
+
+func mustToken(logger *logging.Logger) string {
+	token := flag.String(
+		"token",
+		"",
+		"token for access to telegram bot",
+	)
+
+	flag.Parse()
+
+	if *token == "" {
+		logger.Fatal("token is not specified")
+	}
+
+	return *token
 }
